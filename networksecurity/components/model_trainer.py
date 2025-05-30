23d9984abaa567e3e4707731f3ce71b905fc4fad
off.rkv <off.rkv@gmail.com>
2025-05-30 13:22:33 +0530

    dagsbag added

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -137,6 +137,8 @@ class ModelTrainer:
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
 
+        save_object("final_model/model.pkl",best_model)
+
         ##Model trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
                              train_metric_articfact=classification_train_metric,
